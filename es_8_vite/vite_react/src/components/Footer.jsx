3192a824c0edf3c5a632215ef37d9d66e93467a5
Valerio <valerio.collura@live.it>
2023-12-11 15:39:36 +0100

    aggiunta vite + react

---
+++ es_8_vite/vite_react/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import '../App.css'
+export default function Footer(){
+    return(
+<p className="read-the-docs">
+Click on the Vite and React logos to learn more
+</p>
+)
+}
---
+++ es_8_vite/vite_react/src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import '../App.css'
+
+export default function Main(){
+return(
+<main>
+<div className="card">
+  <h3>/* istruzioni per creare un file vite Con React */</h3>
+  <ul className="lista">
+    <li>da terminale usare comando "npm create vite@latest"</li>
+    <li>selezionare React</li>
+    <li>scegliere se usare Javascript o TypeScript</li>
+  </ul>
+
+  <p>
+    Edit <code>src/App.jsx</code> and save to test HMR
+  </p>
+</div>
+</main>
+)} 
